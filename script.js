e5e3684cab10f1c125b4e648d014342dc08bc8cd
LeoFlawless <18011581477@163.com>
2024-06-18 14:23:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,11 @@ function checkAnswer(questionNumber, answer) {
         1: 4,
         2: 'Paris',
         3: 'The relationship cannot be determined from the information given.',
+        4: '叶某'
     };
 
     if (answer === correctAnswers[questionNumber]) {
-        if (questionNumber === 3) {
+        if (questionNumber === 4) {
             window.location.href = 'success.html';
         } else {
             window.location.href = `question${questionNumber + 1}.html`;
